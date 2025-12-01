cefb16edff877550d2d8a235b8c8715a211f3589
keerthi-mushappanavar <mushappanavarkeerthi@gmail.com>
2025-12-01 16:14:40 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,9 +1,4 @@
 def employee_details(name, emp_id, department, salary):
-    # Remove extra spaces from all string inputs
-    name = name.strip()
-    emp_id = emp_id.strip()
-    department = department.strip()
-
     result = (
         f"Employee Name: {name}\n"
         f"Employee ID: {emp_id}\n"
